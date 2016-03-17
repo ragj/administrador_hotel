6874367621a6c68542ee9ce2f9ba296f6a17fe50
Julio Zebadua <jzebadua@denumeris.com>
2016-03-17 17:09:30 -0600

    Login activado

--- luna/SessionLogin.php
+++ luna/SessionLogin.php
@@ -35,7 +35,7 @@ class SessionLogin extends \Zaphpa\BaseMiddleware {
 
     public function preroute(&$req, &$res) {
 
-        $redirect_after_login = "/welcome";
+        $redirect_after_login = "/";
 
         global $spot;
         $usersMapper = $spot->mapper("Entity\Usuario");
@@ -50,34 +50,10 @@ class SessionLogin extends \Zaphpa\BaseMiddleware {
                 header("Location: http://" . $_SERVER["SERVER_NAME"] . "/login?redirect=" . self::$context["request_uri"]);
                 die();
             } else {
-
                 //  IF USUER IS LOGGED IN
-
                 $req->user = $session->get("user");
-                $allow = false;
-                foreach ($req->user["permisos"] as $permiso) {
-                    if ($permiso["ruta"] == self::$context["pattern"] && $permiso["allow"] == "allow") {
-                        $allow = true;
-                        continue;
-                    }
-                }
-
-                //add notificaciones a usuario
-                $notificaciones = $spot->mapper("Entity\CentroNotificaciones")->query("SELECT * FROM r_usuario_centro_notificacion as uc JOIN d_centro_notificacion as cn "
-                                . "ON cn.id = uc.id_centro_notificacion where uc.id_usuario=" . $req->user['id'] . " and "
-                                . "cn.estado=" . \EstadoNotificacion::generada . " ORDER BY fecha_generada DESC")->toArray();
-                $req->user['notificaciones'] = $notificaciones;
-                $req->user['count_notificaciones'] = (count($notificaciones) > 0) ? count($notificaciones) : '';
-                $req->user['mostra_notificaciones'] = (count($notificaciones) > 0) ? 'block' : 'none';
-
                 $session->set("user", $req->user);
-
-
-                if (!$allow) {
-                    header("Content-Type: application/json;", TRUE, 404);
-                    $out = array("error" => "not allow");
-                    die(json_encode($out));
-                }
+                
             }
         }
 
@@ -85,67 +61,25 @@ class SessionLogin extends \Zaphpa\BaseMiddleware {
             if ($session->get("user", false)) {
                 header("Location: http://" . $_SERVER["SERVER_NAME"] . $redirect_after_login);
             }
-            if (isset($req->data["correo"])) {
-                $username = $req->data["correo"];
+            if (isset($req->data["usuario"])) {
+                $username = $req->data["usuario"];
                 $password = $req->data["password"];
 
-                $user = $usersMapper->where(["correo" => $username]);
+                $user = $usersMapper->where(["usuario" => $username]);
                 if ($user->first()) {
                     if ($user->first()->password === md5($password)) {
 
                         /// LOGIN SUCCESS
 
                         $user = $user->first()->toArray();
-
-
-                        $rolMapper = $spot->mapper("Entity\Rol");
-                        $user["roles"] = $rolMapper->where(["id" => $user["roles"]])->with("permisos")->toArray();
-
-                        $user["permisos"] = [];
-                        foreach ($user["roles"] as $rol) {
-                            $user["permisos"] = array_merge($user["permisos"], $rol["permisos"]);
-                        }
-
-
-                        /* eliminar permisos dobles */
-                        $temp;
-                        foreach ($user["permisos"] as $per) {
-                            $temp[] = ['ruta' => $per['ruta'], 'allow' => $per['allow']];
-                        }
-                        $user["permisos"] = array_map("unserialize", array_unique(array_map("serialize", $temp)));
-
-
-                        /* menu */
-                        $menuMapper = $spot->mapper("Entity\MenuAcl");
-                        $pMenu = $menuMapper->where(['parent' => NULL])->with("acl")->order(['order' => 'ASC'])->toArray();
-
-                        $menu = [];
-                        foreach ($pMenu as $p) {
-                            if ($this->tienePermiso($user["permisos"], $p['acl']['ruta'])) {
-                                $menu[] = ['id' => $p['id'], 'class' => $p['class'], 'label' => $p['label'], 'ruta' => $p['acl']['ruta']];
-                            }
-                        }
-
-                        foreach ($menu as &$m) {
-                            $subm = $menuMapper->where(['parent' => $m['id']])->with("acl")->order(['order' => 'ASC'])->toArray();
-                            foreach ($subm as $sm) {
-                                if ($this->tienePermiso($user["permisos"], $sm['acl']['ruta'])) {
-                                    $m['sub'][] = ['id' => $sm['id'], 'class' => $sm['class'], 'label' => $sm['label'], 'ruta' => $sm['acl']['ruta']];
-                                }
-                            }
-                        }
-
-                        $user['menu'] = $menu;
                         $session->set("user", $user);
-
-                        $redirect_after_login = ($user['id_tipo_usuario'] == \TipoUsuario::psv) ? '/inicio' : $redirect_after_login;
-
 
                         if (isset($req->data["redirect"])) {
                             header("Location: http://" . $_SERVER["SERVER_NAME"] . $req->data["redirect"]);
                         } else {
                             header("Location: http://" . $_SERVER["SERVER_NAME"] . $redirect_after_login);
                         }
+
                     } else {
                         $session_controller = $session_handle->getSegment('Luna\Controllers');
                         $session_controller->setFlash("alert", ["message" => "El password y el usuario no coinciden!", "status" => "Error:", "class" => "alert-danger"]);
--- model/Usuario.php
+++ model/Usuario.php
@@ -20,23 +20,14 @@ class Usuario extends \Spot\Entity {
         return [
             'id' => ['type' => 'integer', 'primary' => true, 'autoincrement' => true],
             'nombre' => ['type' => 'string', 'required' => true],
-            'correo' => ['type' => 'string', 'required' => true],
-            'password' => ['type' => 'string', 'required' => true],
             'papellido' => ['type' => 'string', 'required' => true],
-            'sapellido' => ['type' => 'string', 'required' => true],
-            'id_tipo_usuario' => ['type' => 'integer', 'required' => true],
-            'estado' => ['type' => 'integer', 'required' => true],
-            'roles' => ['type' => 'array'],
+            'usuario' => ['type' => 'string', 'required' => true],
+            'password' => ['type' => 'string', 'required' => true],
             'create_at' => ['type' => 'datetime', 'required' => false],
         ];
     }
 
-    public static function relations(Mapper $mapper, Entity $entity) {
-        return [
-            'tipo' => $mapper->belongsTo($entity, 'Entity\TipoUsuario', 'id_tipo_usuario')
-        ];
-    }
-
+    
     public static function events(EventEmitter $eventEmitter) {
         $eventEmitter->on('beforeInsert', function (Entity $entity, Mapper $mapper) {
             $entity->password = md5($entity->password);
