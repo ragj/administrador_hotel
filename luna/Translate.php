728334fff0863fe586d36633f5498fdcd2032187
Julio Zebadua <jzebadua@denumeris.com>
2016-04-15 14:46:11 -0500

    Error en las rutas

--- luna/Translate.php
+++ luna/Translate.php
@@ -6,146 +6,137 @@ namespace Luna;
  * @package Luna
  *
  * Usage:
- * 
+ *
  * $router->attach('\Luna\SessionLogin');
- *     
- * 
+ *
+ *
  */
 class Translate extends \Zaphpa\BaseMiddleware {
 
-    public static $lang = "";
-    public $default_languaje = 'es';
-    public $languajes = ['es', 'en', 'jp', 'ch'];
-    public static $uri = "";
-    public static $pattern = "";
+	public static $lang = "";
+	public $default_languaje = 'en';
+	public $languajes = ['es', 'en', 'jp', 'ch'];
+	public static $uri = "";
+	public static $pattern = "";
 
-    static function removeLang( $uri ){
+	static function removeLang($uri) {
 
-    }
+	}
 
-    static public function uri( $uri = "" ) {
-        $uri = $uri == "" ?$_SERVER["REQUEST_URI"]:$uri;
-        $lang = isset($_GET["lang"]) ? $_GET["lang"] : explode('/', $uri)[1];
-        if (strlen($lang) == 2 && str_replace("/" . $lang, "/", $uri) != $uri) {
-            $uri_out = substr($uri, 3);
-        } else {
-            $uri_out = $uri;
-        }                
-        $tokens = parse_url('http://foo.com' . $uri_out);
-        $uri_out = rawurldecode($tokens['path']);
-        self::$uri = $uri_out;
-        return $uri_out;
-    }
+	static public function uri($uri = "") {
+		$uri = $uri == "" ? $_SERVER["REQUEST_URI"] : $uri;
+		$lang = isset($_GET["lang"]) ? $_GET["lang"] : explode('/', $uri)[1];
+		if (strlen($lang) == 2 && str_replace("/" . $lang, "/", $uri) != $uri) {
+			$uri_out = substr($uri, 3);
+		} else {
+			$uri_out = $uri;
+		}
+		$tokens = parse_url('http://foo.com' . $uri_out);
+		$uri_out = rawurldecode($tokens['path']);
+		self::$uri = $uri_out;
+		return $uri_out;
+	}
 
-    //  Regresa la URL actual en el idioma especificado
-    // $uriHandler => array ( uri , mapper => "{param}" )
-    static function to( $lang_to , $uriHandler = false ){
+	//  Regresa la URL actual en el idioma especificado
+	// $uriHandler => array ( uri , mapper => "{param}" )
+	static function to($lang_to, $uriHandler = false) {
 
-        global $ROUTES;
-        global $BASE;
-        $url="";
-        //si existe la uri en las rutas
-        if( isset($ROUTES[self::$uri]) ){
-          $final=$ROUTES[self::$uri];
-          if(isset($final[$lang_to])){
-                $url=$BASE."/".$lang_to.$final[$lang_to];
-                return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-            }
-            else{
-                $url=$BASE."/".$lang_to.$final["path"];
-                return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-            }
-        } 
-        //si existe el pattern en las rutas
-        else if(isset($ROUTES[self::$context["pattern"]])){
-          $final=$ROUTES[self::$context["pattern"]];
-          if(isset($final)){
-                if(isset($final[$lang_to])){
-                    $url=$BASE."/".$lang_to.$final[$lang_to];
-                    return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-                }
-                else{
-                    $url=$BASE."/".$lang_to.$final["path"];
-                    return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-                }    
-            }
-        }
-        else{
-        //sino buscamos dentro de cada elementos del arreglo ROUTES
-            foreach ($ROUTES as $key => $route) {
-                //si existe alguna ruta en el lenguaje actual y si el valor de la ruta es igual al uri recibido
-                if(isset($route[self::$lang])&&$route[self::$lang]==self::$uri){
-                        //si existe ruta en el lenguaje actual
-                    if(isset($route[$lang_to])){
-                        $url=$BASE."/".$lang_to.$route[$lang_to];
-                        return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-                        break;
-                    }
-                    else{
-                        $url=$BASE."/".$lang_to.$route["path"];
-                        return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-                        break;
-                    }
-                }
-                else if(isset($route[self::$lang])&&$route[self::$lang]==self::$context["pattern"]){
-                        //si existe ruta en el lenguaje actual
-                    if(isset($route[$lang_to])){
-                        $url=$BASE."/".$lang_to.$route[$lang_to];
-                        return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-                        break;
-                    }
-                    else{
-                        $url=$BASE."/".$lang_to.$route["path"];
-                        return !$uriHandler?$url:str_replace( $uriHandler["mapper"] , $uriHandler["uri"], $url);
-                        break;
-                    }
-                }
-                else{
-                    $url="";
-                }
-            }
-            
-        }
-          
-        
-        return $url;  
-        
-    }
+		global $ROUTES;
+		global $BASE;
+		$url = "";
+		//si existe la uri en las rutas
+		if (isset($ROUTES[self::$uri])) {
+			$final = $ROUTES[self::$uri];
+			if (isset($final[$lang_to])) {
+				$url = $BASE . "/" . $lang_to . $final[$lang_to];
+				return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+			} else {
+				$url = $BASE . "/" . $lang_to . $final["path"];
+				return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+			}
+		}
+		//si existe el pattern en las rutas
+		else if (isset($ROUTES[self::$context["pattern"]])) {
+			$final = $ROUTES[self::$context["pattern"]];
+			if (isset($final)) {
+				if (isset($final[$lang_to])) {
+					$url = $BASE . "/" . $lang_to . $final[$lang_to];
+					return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+				} else {
+					$url = $BASE . "/" . $lang_to . $final["path"];
+					return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+				}
+			}
+		} else {
+			//sino buscamos dentro de cada elementos del arreglo ROUTES
+			foreach ($ROUTES as $key => $route) {
+				//si existe alguna ruta en el lenguaje actual y si el valor de la ruta es igual al uri recibido
+				if (isset($route[self::$lang]) && $route[self::$lang] == self::$uri) {
+					//si existe ruta en el lenguaje actual
+					if (isset($route[$lang_to])) {
+						$url = $BASE . "/" . $lang_to . $route[$lang_to];
+						return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+						break;
+					} else {
+						$url = $BASE . "/" . $lang_to . $route["path"];
+						return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+						break;
+					}
+				} else if (isset($route[self::$lang]) && $route[self::$lang] == self::$context["pattern"]) {
+					//si existe ruta en el lenguaje actual
+					if (isset($route[$lang_to])) {
+						$url = $BASE . "/" . $lang_to . $route[$lang_to];
+						return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+						break;
+					} else {
+						$url = $BASE . "/" . $lang_to . $route["path"];
+						return !$uriHandler ? $url : str_replace($uriHandler["mapper"], $uriHandler["uri"], $url);
+						break;
+					}
+				} else {
+					$url = "";
+				}
+			}
 
-    public function preprocess(&$router) {
+		}
 
-        //self::$pattern = self::$context["pattern"];
-        $lang = isset($_GET["lang"]) ? $_GET["lang"] : explode('/', $_SERVER["REQUEST_URI"])[2];
-        self::$lang = (in_array($lang, $this->languajes)) ? $lang : $this->default_languaje;
-        global $ROUTES;
-        foreach ($ROUTES as $key => $route) {
-            if(isset($route[self::$lang]) ){
-                $route["path"] = $route[self::$lang];
-                $router->addRoute($route);
-            }
-        }
-                    
-    }
+		return $url;
 
-    public function preroute(&$req, &$res) {
-        $req->lang = self::$lang;
-    }
+	}
 
-    static function url($uri){
-        global $BASE;
-        global $ROUTES;
-        $lang = self::$lang?"/".self::$lang:"";
-        if( isset($ROUTES[$uri]) ){
-            $final=$ROUTES[$uri];
-            if(isset($final[self::$lang])){
-                $url=$BASE.$lang.$final[self::$lang];
-            }
-            else{
-                $url=$BASE.$lang.$final["path"];
-            }
-        }
-        return $url;
-    }
+	public function preprocess(&$router) {
+
+		//self::$pattern = self::$context["pattern"];
+		$lang = isset($_GET["lang"]) ? $_GET["lang"] : explode('/', $_SERVER["REQUEST_URI"])[2];
+		self::$lang = (in_array($lang, $this->languajes)) ? $lang : $this->default_languaje;
+		global $ROUTES;
+		foreach ($ROUTES as $key => $route) {
+			if (isset($route[self::$lang])) {
+				$route["path"] = $route[self::$lang];
+				$router->addRoute($route);
+			}
+		}
+
+	}
+
+	public function preroute(&$req, &$res) {
+		$req->lang = self::$lang;
+	}
+
+	static function url($uri) {
+		global $BASE;
+		global $ROUTES;
+		$lang = self::$lang ? "/" . self::$lang : "";
+		if (isset($ROUTES[$uri])) {
+			$final = $ROUTES[$uri];
+			if (isset($final[self::$lang])) {
+				$url = $BASE . $lang . $final[self::$lang];
+			} else {
+				$url = $BASE . $lang . $final["path"];
+			}
+		}
+		return $url;
+	}
 
 }
 
--- luna/router/routes.php
+++ luna/router/routes.php
@@ -10,259 +10,254 @@
 global $ROUTES;
 //Transaleted routes
 $ROUTES = [
-    "/" => [
-        'es' => '/inicio',
-        'en' => '/home',
-        'allow' => true,
-        'path' => '/',
-        'get' => array('Plain', 'home')
-        ],
-    "/about-us" => [
-        'es' => "/nosotros",
-        'en' => "/about-us",
-        'allow' => true,
-        'path' => '/about-us',
-        'get' => array('Plain', 'about')
-    ],
-    "/hotel-collection" => [
-        'es' => "/hoteles",
-        'en' => "/hotel-collection",
-        'allow' => true,
-        'path' => '/hotel-collection',
-        'get' => array('Plain', 'hotel')
-    ],
-    "/hotel-collection/{hotel}"=>[
-        'es' => "/hoteles/{hotel}",
-        'en' => "/hotel-collection/{hotel}",
-        'allow' => true,
-        'path' => '/hotel-collection/{hotel}',
-        'get' => array('Plain', 'hotel')
-    ],
-    "/experience"=>[
-        'es'=>"/experiencia",
-        'en'=>"/experience",
-        'allow' => true,
-        'path' => '/experience',
-        'get' => array('Plain', 'experience')
-    ],
-    "/experience/{exper}"=>[
-        'es'=>"/experiencia/{exper}",
-        'en'=>"/experience/{exper}",
-        'allow' => true,
-        'path' => '/experience/{exper}',
-        'get' => array('Plain', 'experience')
-    ],
-    "/transfer"=>[
-        'es'=>"/transfer",
-        'en'=>"/transfer",
-        'path' => '/transfer',
-        'get' => array('Plain', 'transfer')
-    ],
-    "/contact-us"=>[
-        'es'=>"/contactanos",
-        'en'=>"/contact-us",
-        'allow' => true,
-        'path' => '/contact-us',
-        'get' => array('Plain', 'contact'),
-        'post' => array('Plain', 'contact')
-    ],
-    "/transfer"=>[
-        'es'=>"/olividada",
-        'en'=>"/forgot",
-        'allow' => true,
-        'path' => '/forgot',
-        'get' => array('Plain', 'forgot'),
-        'post' => array('Plain', 'forgot')
-    ],
-    "/forgot/{uid}"=>[
-        'es'=>"/olvidada/{uid}",
-        'en'=>"/forgot/{uid}",
-        'allow' => true,
-        'path' => '/forgot/{uid}',
-        'get' => array('Plain', 'change')
-    ],
-    "/register"=>[
-        'es'=>"/registro",
-        'en'=>"/register",
-        'allow' => true,
-        'path' => '/register',
-        'get' => array('Plain', 'register'),
-        'post' => array('Plain', 'register')
-    ],
-    "/aviso"=>[
-        'path' => '/aviso',
-        'allow' => true,
-        'get' => array('Plain', 'aviso')
-    ],
-    "/panel/hotel/add"=>[
-        'path' => '/panel/hotel/add',
-        'get' => array('Hotel', 'add'),
-        'post' => array('Hotel', 'add')
-    ],
-    "/login"=>[
-        'es' => '/login',
-        'en' => '/login',
-        'path' => '/login',
-        'get' => array('Usuario', 'login'),
-        'post' => array('Usuario', 'login')
-    ]
+	"/" => [
+		'es' => '/inicio',
+		'en' => '/home',
+		'allow' => true,
+		'path' => '/',
+		'get' => array('Plain', 'home'),
+	],
+	"/about-us" => [
+		'es' => "/nosotros",
+		'en' => "/about-us",
+		'allow' => true,
+		'path' => '/about-us',
+		'get' => array('Plain', 'about'),
+	],
+	"/hotel-collection" => [
+		'es' => "/hoteles",
+		'en' => "/hotel-collection",
+		'allow' => true,
+		'path' => '/hotel-collection',
+		'get' => array('Plain', 'hotel'),
+	],
+	"/hotel-collection/{hotel}" => [
+		'es' => "/hoteles/{hotel}",
+		'en' => "/hotel-collection/{hotel}",
+		'allow' => true,
+		'path' => '/hotel-collection/{hotel}',
+		'get' => array('Plain', 'hotel'),
+	],
+	"/experience" => [
+		'es' => "/experiencia",
+		'en' => "/experience",
+		'allow' => true,
+		'path' => '/experience',
+		'get' => array('Plain', 'experience'),
+	],
+	"/experience/{exper}" => [
+		'es' => "/experiencia/{exper}",
+		'en' => "/experience/{exper}",
+		'allow' => true,
+		'path' => '/experience/{exper}',
+		'get' => array('Plain', 'experience'),
+	],
+	"/transfer" => [
+		'es' => "/transfer",
+		'en' => "/transfer",
+		'path' => '/transfer',
+		'get' => array('Plain', 'transfer'),
+	],
+	"/contact-us" => [
+		'es' => "/contactanos",
+		'en' => "/contact-us",
+		'allow' => true,
+		'path' => '/contact-us',
+		'get' => array('Plain', 'contact'),
+		'post' => array('Plain', 'contact'),
+	],
+	"/forgot" => [
+		'es' => "/olividada",
+		'en' => "/forgot",
+		'allow' => true,
+		'path' => '/forgot',
+		'get' => array('Plain', 'forgot'),
+		'post' => array('Plain', 'forgot'),
+	],
+	"/forgot/{uid}" => [
+		'es' => "/olvidada/{uid}",
+		'en' => "/forgot/{uid}",
+		'allow' => true,
+		'path' => '/forgot/{uid}',
+		'get' => array('Plain', 'change'),
+	],
+	"/register" => [
+		'es' => "/registro",
+		'en' => "/register",
+		'allow' => true,
+		'path' => '/register',
+		'get' => array('Plain', 'register'),
+		'post' => array('Plain', 'register'),
+	],
+	"/aviso" => [
+		'path' => '/aviso',
+		'allow' => true,
+		'get' => array('Plain', 'aviso'),
+	],
+	"/panel/hotel/add" => [
+		'path' => '/panel/hotel/add',
+		'get' => array('Hotel', 'add'),
+		'post' => array('Hotel', 'add'),
+	],
+	"/login" => [
+		'es' => '/login',
+		'en' => '/login',
+		'path' => '/login',
+		'get' => array('Usuario', 'login'),
+		'post' => array('Usuario', 'login'),
+	],
 ];
 
 foreach ($ROUTES as $key => $route) {
-    $router->addRoute( $route );
+	$router->addRoute($route);
 }
-
 
 //admin routes
 $router->addRoute(array(
-    'path' => '/panel/hotel/show',
-    'get' => array('Hotel', 'show'),
+	'path' => '/panel/hotel/show',
+	'get' => array('Hotel', 'show'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/hotel/edit/{hotel}',
-    'get' => array('Hotel', 'edit'),
-    'post' => array('Hotel', 'edit')
+	'path' => '/panel/hotel/edit/{hotel}',
+	'get' => array('Hotel', 'edit'),
+	'post' => array('Hotel', 'edit'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/hotel/delete/{hotel}',
-    'get' => array('Hotel', 'delete')
+	'path' => '/panel/hotel/delete/{hotel}',
+	'get' => array('Hotel', 'delete'),
 ));
-
 
 ///HotelImages Routes
 $router->addRoute(array(
-    'path' => '/panel/hotel/addImages',
-    'get' => array('Hotel', 'addImages'),
-    'post' => array('Hotel', 'addImages')
+	'path' => '/panel/hotel/addImages',
+	'get' => array('Hotel', 'addImages'),
+	'post' => array('Hotel', 'addImages'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/hotel/editImages/{hotel}',
-    'get' => array('Hotel', 'editImages'),
-    'post' => array('Hotel', 'editImages')
+	'path' => '/panel/hotel/editImages/{hotel}',
+	'get' => array('Hotel', 'editImages'),
+	'post' => array('Hotel', 'editImages'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/hotel/deleteImages/{hotel}',
-    'get' => array('Hotel', 'deleteImages')
+	'path' => '/panel/hotel/deleteImages/{hotel}',
+	'get' => array('Hotel', 'deleteImages'),
 ));
-
 
 ///Tour Routes
 $router->addRoute(array(
-    'path' => '/panel/tour/add',
-    'get' => array('Tour', 'add'),
-    'post'=>array('Tour','add')
+	'path' => '/panel/tour/add',
+	'get' => array('Tour', 'add'),
+	'post' => array('Tour', 'add'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/tour/show',
-    'get' => array('Tour', 'show')
+	'path' => '/panel/tour/show',
+	'get' => array('Tour', 'show'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/tour/edit/{exper}',
-    'get' => array('Tour', 'edit'),
-    'post' => array('Tour', 'edit')
+	'path' => '/panel/tour/edit/{exper}',
+	'get' => array('Tour', 'edit'),
+	'post' => array('Tour', 'edit'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/tour/delete/{exper}',
-    'get' => array('Tour', 'delete')
+	'path' => '/panel/tour/delete/{exper}',
+	'get' => array('Tour', 'delete'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/tour/delete',
-    'get' => array('Tour', 'show')
+	'path' => '/panel/tour/delete',
+	'get' => array('Tour', 'show'),
 ));
 
 ///TourImages Routes
 $router->addRoute(array(
-    'path' => '/panel/tour/addImages',
-    'get' => array('Tour', 'addImages'),
-    'post' => array('Tour', 'addImages')
+	'path' => '/panel/tour/addImages',
+	'get' => array('Tour', 'addImages'),
+	'post' => array('Tour', 'addImages'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/tour/editImages/{exper}',
-    'get' => array('Tour', 'editImages'),
-    'post' => array('Tour', 'editImages')
+	'path' => '/panel/tour/editImages/{exper}',
+	'get' => array('Tour', 'editImages'),
+	'post' => array('Tour', 'editImages'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/tour/deleteImages/{exper}',
-    'get' => array('Tour', 'deleteImages')
+	'path' => '/panel/tour/deleteImages/{exper}',
+	'get' => array('Tour', 'deleteImages'),
 ));
 
 ///User Routes
 $router->addRoute(array(
-    'path' => '/panel/user/add',
-    'get' => array('Usuario', 'add'),
-    'post'=>array('Usuario','add')
+	'path' => '/panel/user/add',
+	'get' => array('Usuario', 'add'),
+	'post' => array('Usuario', 'add'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/user/show',
-    'get' => array('Usuario', 'show')
+	'path' => '/panel/user/show',
+	'get' => array('Usuario', 'show'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/user/edit/{exper}',
-    'get' => array('Usuario', 'edit'),
-    'post' => array('Usuario', 'edit')
+	'path' => '/panel/user/edit/{exper}',
+	'get' => array('Usuario', 'edit'),
+	'post' => array('Usuario', 'edit'),
 ));
 
 $router->addRoute(array(
-    'path' => '/panel/user/active/{exper}',
-    'get' => array('Usuario', 'active'),
+	'path' => '/panel/user/active/{exper}',
+	'get' => array('Usuario', 'active'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/user/delete/{exper}',
-    'get' => array('Usuario', 'delete')
+	'path' => '/panel/user/delete/{exper}',
+	'get' => array('Usuario', 'delete'),
 ));
 
 //contact routes
 $router->addRoute(array(
-    'path' => '/panel/contact/show',
-    'get' => array('Contact', 'show')
+	'path' => '/panel/contact/show',
+	'get' => array('Contact', 'show'),
 ));
 
 //transfer Blocks routes
 $router->addRoute(array(
-    'path' => '/panel/transfer/addBlock',
-    'get' => array('Transfer', 'addBlock'),
-    'post'=>array('Transfer','addBlock')
+	'path' => '/panel/transfer/addBlock',
+	'get' => array('Transfer', 'addBlock'),
+	'post' => array('Transfer', 'addBlock'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/transfer/addDetail',
-    'get' => array('Transfer','addDetail'),
-    'post'=>array('Transfer','addDetail')
+	'path' => '/panel/transfer/addDetail',
+	'get' => array('Transfer', 'addDetail'),
+	'post' => array('Transfer', 'addDetail'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/transfer/listBlock',
-    'get' => array('Transfer', 'listBlock')
+	'path' => '/panel/transfer/listBlock',
+	'get' => array('Transfer', 'listBlock'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/transfer/editBlock/{block}',
-    'get' => array('Transfer','editBlock'),
-    'post'=>array('Transfer','editBlock')
+	'path' => '/panel/transfer/editBlock/{block}',
+	'get' => array('Transfer', 'editBlock'),
+	'post' => array('Transfer', 'editBlock'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/transfer/delete/{block}',
-    'get' => array('Transfer','deleteBlock')
+	'path' => '/panel/transfer/delete/{block}',
+	'get' => array('Transfer', 'deleteBlock'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/transfer/editDetail/{detail}',
-    'get' => array('Transfer','editDetail'),
-    'post'=>array('Transfer','editDetail')
+	'path' => '/panel/transfer/editDetail/{detail}',
+	'get' => array('Transfer', 'editDetail'),
+	'post' => array('Transfer', 'editDetail'),
 ));
 $router->addRoute(array(
-    'path' => '/panel/transfer/deleteDetail/{detail}',
-    'get' => array('Transfer','deleteDetail')
+	'path' => '/panel/transfer/deleteDetail/{detail}',
+	'get' => array('Transfer', 'deleteDetail'),
 ));
-
-
 
 ?>
