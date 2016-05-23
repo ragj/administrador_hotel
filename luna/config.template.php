45c6ce43fe94371bfffacb73541188cdd5da3a7b
Guillermo Escalona <gescalona@denumeris.com>
2016-05-23 17:52:02 -0500

    mails

--- luna/config.template.php
+++ luna/config.template.php
@@ -24,13 +24,20 @@
 	global $session_handle;
     $session_handle = $aura_session->newInstance($_SESSION);
 
+     global $cmail;
+    //$cmail="bali@lozano.com";
+    $cmail="gescalona@denumeris.com";
+    global $ccmail;
+    //$ccmail="alex.mendiola@lozano.com";
+    $ccmail="geoshada@gmail.com";
+
     include __DIR__."/mail.config.php";
 
 	// LANG  MANNAGER
 	// TODO: Pass to middleware
-	$lang = isset($_GET["lang"])?$_GET["lang"]:"es";
-	if( str_replace( "/".$lang ,"",$_SERVER["REQUEST_URI"] ) != $_SERVER["REQUEST_URI"] ){
-		$_SERVER["REQUEST_URI"] = substr( $_SERVER["REQUEST_URI"], 3 );	
-	}
+	// $lang = isset($_GET["lang"])?$_GET["lang"]:"es";
+	// if( str_replace( "/".$lang ,"",$_SERVER["REQUEST_URI"] ) != $_SERVER["REQUEST_URI"] ){
+	// 	$_SERVER["REQUEST_URI"] = substr( $_SERVER["REQUEST_URI"], 3 );	
+	// }
 
 ?>
